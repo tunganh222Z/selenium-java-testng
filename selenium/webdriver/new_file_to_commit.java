b9ba10620ce8e0474ccd741d62ba47429fbf9b90
tunganh222Z <tunganh97tuaf@gmail.com>
2024-07-30 22:51:33 +0700

    Initial commit

---
+++ selenium/webdriver/new_file_to_commit.java
@@ -0,0 +1,11 @@
+package webdriver;
+
+import java.util.Scanner;
+
+public class new_file_to_commit {
+    public static void  main(string[] agrs){
+        Scanner sc = new Scanner();
+        int n = sc.nextInt();
+        System.out.println("test thử commit" + n);
+    }
+}
