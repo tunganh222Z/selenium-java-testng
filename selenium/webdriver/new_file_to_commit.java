fef5fd175794d160ad25578dcf25f5cd550680e4
tunganh222Z <tunganh97tuaf@gmail.com>
2024-07-30 23:08:51 +0700

    delete .idea

--- selenium/webdriver/new_file_to_commit.java
+++ selenium/webdriver/new_file_to_commit.java
@@ -3,7 +3,7 @@ package webdriver;
 import java.util.Scanner;
 
 public class new_file_to_commit {
-    public static void  main(string[] agrs){
+    public static void  main(String[] agrs){
         Scanner sc = new Scanner();
         int n = sc.nextInt();
         System.out.println("test thử commit" + n);
