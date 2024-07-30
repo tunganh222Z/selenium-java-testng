5ddb723706b35d2187546037d5a67c4acedb47b9
tunganh222Z <tunganh97tuaf@gmail.com>
2024-07-30 22:39:35 +0700

    Initial commit

---
+++ selenium/webdriver/Person.java
@@ -0,0 +1,10 @@
+package webdriver;
+
+import java.util.Scanner;
+
+public class Person {
+    public static void main (String[] agrs){
+        Scanner sc = new Scanner (System.in);
+
+    }
+}
---
+++ selenium/webdriver/Topic_01_Enviroment.java
@@ -0,0 +1,32 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic_01_Enviroment {
+    WebDriver driver;
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
