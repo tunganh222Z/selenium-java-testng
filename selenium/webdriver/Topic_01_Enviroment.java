ab161340589c7cdce42e53ca512d1c27bb867abb
tunganh222Z <tunganh97tuaf@gmail.com>
2024-07-31 23:24:48 +0700

    delete .idea

--- selenium/webdriver/Topic_01_Enviroment.java
+++ selenium/webdriver/Topic_01_Enviroment.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+
+
 public class Topic_01_Enviroment {
     WebDriver driver;
 
