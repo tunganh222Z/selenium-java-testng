b4cd69cabee1d7fa27a80762b4ab9dd916015084
tunganh222Z <tunganh97tuaf@gmail.com>
2024-09-05 00:25:35 +0700

    Topic_27_Wait_03_Implicit

---
+++ selenium/webdriver/Topic_27_Wait_03_Implicit.java
@@ -0,0 +1,60 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_27_Wait_03_Implicit {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+
+        driver.manage().window();
+    }
+
+    @Test
+    public void TC_01_Equal_5s(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+
+    }
+
+    @Test
+    public void TC_02_LessThan_5s(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+
+    }
+
+    @Test
+    public void TC_03_GreaterThan_5s(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+}
