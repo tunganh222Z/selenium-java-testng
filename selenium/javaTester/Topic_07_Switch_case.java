889c9b565443392c025b87d52335264d12af9857
tunganh222Z <tunganh97tuaf@gmail.com>
2024-09-14 11:33:36 +0700

    Topic_08_Array

---
+++ selenium/javaTester/Topic_07_Switch_case.java
@@ -0,0 +1,4 @@
+package javaTester;
+
+public class Topic_07_Switch_case {
+}
---
+++ selenium/javaTester/Topic_08_Array.java
@@ -0,0 +1,18 @@
+package javaTester;
+
+import org.testng.annotations.Test;
+
+import java.util.Scanner;
+
+public class Topic_08_Array {
+    Scanner sc = new Scanner(System.in);
+
+    @Test
+    public void TC_01_Array() {
+        int[] number = new int[5];
+        for (int i = 0; i < 5; i++) {
+            number[i] = sc.nextInt();
+        }
+    }
+
+}
