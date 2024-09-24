636745ab13fbdacb8525c4ec7e0553a688b67d04
tunganh222Z <tunganh97tuaf@gmail.com>
2024-09-25 00:55:55 +0700

    Topic_18_Try_Catch_Throw_Custom_Exception

---
+++ selenium/javaTester/Exception/HandleException.java
@@ -0,0 +1,12 @@
+package javaTester.Exception;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class HandleException {
+    public static void main(String[] args) {
+
+        
+
+    }
+}
---
+++ selenium/javaTester/Exception/TryCatchException.java
@@ -0,0 +1,42 @@
+package javaTester.Exception;
+
+public class TryCatchException {
+    public static void main(String[] args) {
+        int number = 10;
+
+        try {
+            // Đúng : chạy code block trong Try và không qua catch
+            // Sai : Gặp exception thì nhảy qua Catch
+
+            number = number / 0 ;
+        } catch (ArithmeticException e){
+            e.printStackTrace();
+        }
+
+        System.out.println(number);
+
+        String[] browserName = {"Chrome", "Firefox", "Safari"};
+        try {
+            browserName[0] = "Edge Chromium";
+            browserName[3] = "Fail out of bounds";
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            // Dù pass hay fail thì cũng phải chạy code trong này
+        }
+
+        for (String browser : browserName){
+            System.out.println(browser);
+        }
+
+    }
+
+    public static void sleepInSecond (long timeout) {
+        try {
+            Thread.sleep(timeout * 1000);
+
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
